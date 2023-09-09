3f39e0cc8cdb14b887fd49151826d12eb97bca42
LuizRedGalaxy <luizgustavoglt@gmail.com>
2023-09-09 16:38:03 -0300

    [5]: Adicionado rota add produto

--- src/app/painel/produtos/page.tsx
+++ src/app/painel/produtos/page.tsx
@@ -1,9 +1,14 @@
 import SectionProducts from "@/components/sections/products/products";
+import Link from "next/link";
+import { Button } from "@mui/material";
 
 export default function PageProducts() {
   return (
     <main>
       <SectionProducts />
+      <Link href="/painel/produtos/add-produto">
+        <Button variant="contained">Add - Produto</Button>
+      </Link>
     </main>
   );
 }
---
+++ src/components/sections/add-product/add-product.tsx
@@ -0,0 +1,15 @@
+import { Container } from "@mui/material";
+
+export type DataSectionAddProduct = {};
+
+export default function SectionAddProduct(data: DataSectionAddProduct) {
+  const {} = data;
+
+  return (
+    <section>
+      <Container>
+        <h2>Desenvolver seção Add Produto !</h2>
+      </Container>
+    </section>
+  );
+}
