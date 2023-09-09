64e0adca60fe9d2504d404a3c57c1601d5b491c4
LuizRedGalaxy <luizgustavoglt@gmail.com>
2023-09-09 16:52:44 -0300

    [5]: Desenvolvido rota configurações

---
+++ src/app/painel/configuracoes/page.tsx
@@ -0,0 +1,9 @@
+import SectionSettings from "@/components/sections/settings/settings";
+
+export default function PageSettings(){
+  return (
+    <main>
+      <SectionSettings/>
+    </main>
+  );
+}
---
+++ src/components/sections/settings/settings.tsx
@@ -0,0 +1,15 @@
+import { Container } from "@mui/material";
+
+export type DataSectionSetting = {};
+
+export default function SectionSettings(data: DataSectionSetting) {
+  const {} = data;
+
+  return (
+    <section>
+      <Container>
+        <h2>Desenvolver Seção Configurações !</h2>
+      </Container>
+    </section>
+  );
+}
