ce670e59777860d6f766fdeb4b7030e9858fe2d5
LuizRedGalaxy <luizgustavoglt@gmail.com>
2023-09-09 18:21:43 -0300

    [5]: Desenvolvido rota finanças

---
+++ src/app/painel/financa/page.tsx
@@ -0,0 +1,9 @@
+import SectionFinance from "@/components/sections/finance/finance";
+
+export default function PageFinance() {
+  return (
+    <main>
+      <SectionFinance />
+    </main>
+  );
+}
---
+++ src/components/sections/finance/finance.tsx
@@ -0,0 +1,15 @@
+import { Container } from "@mui/material";
+
+export type DataSectionFinance = {};
+
+export default function SectionFinance(data: DataSectionFinance) {
+  const {} = data;
+
+  return (
+    <section>
+      <Container>
+        <h2>Desenvolver seção finanças !</h2>
+      </Container>
+    </section>
+  );
+}
