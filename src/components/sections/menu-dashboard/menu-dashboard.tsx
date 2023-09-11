513496a85fd0e4003a4d4e528dab7190b5b5b0e5
LuizRedGalaxy <luizgustavoglt@gmail.com>
2023-09-11 12:23:38 -0300

    Adicionado prototipo para mudar thema escuro e claro

--- src/components/sections/menu-dashboard/menu-dashboard.tsx
+++ src/components/sections/menu-dashboard/menu-dashboard.tsx
@@ -2,8 +2,6 @@
 
 import style from "./menu-dashboard.module.scss";
 import React, { useEffect } from "react";
-import { Button, Stack } from "@mui/material";
-import logo from "@/assets/imgs/logo.svg";
 import mainIcon from "@/assets/imgs/icon.svg";
 import Link from "next/link";
 import HomeIcon from "@mui/icons-material/Home";
@@ -12,13 +10,19 @@ import LocalGroceryStoreIcon from "@mui/icons-material/LocalGroceryStore";
 import InventoryIcon from "@mui/icons-material/Inventory";
 import StoreIcon from "@mui/icons-material/Store";
 import LocalAtmIcon from "@mui/icons-material/LocalAtm";
+import SettingsIcon from "@mui/icons-material/Settings";
+import WbIncandescentIcon from "@mui/icons-material/WbIncandescent";
+import WbIncandescentOutlinedIcon from "@mui/icons-material/WbIncandescentOutlined";
 import { useRouter } from "next/router";
+
+import logo from "@/assets/imgs/logo.svg";
+import logoDark from "@/assets/imgs/logo-dark.svg";
 
 export type DataSectionMenuDashBoard = {
   initialPage: string;
 };
 
-type DataMenuOption = {
+type DataMenuOptionPage = {
   text: string;
   link: string;
   icon: JSX.Element;
@@ -26,10 +30,16 @@ type DataMenuOption = {
   setPage: (page: string) => void;
 };
 
-function MenuOption({ icon, link, text, active, setPage }: DataMenuOption) {
+function MenuOptionPage({
+  icon,
+  link,
+  text,
+  active,
+  setPage,
+}: DataMenuOptionPage) {
   return (
     <Link
-      className={`${style.menuOption} ${active ? style.active : ""}`}
+      className={`${style.menuOptionPage} ${active ? style.active : ""}`}
       href={`/painel/${link}`}
       onClick={() => {
         setPage(link);
@@ -45,16 +55,47 @@ function MenuOption({ icon, link, text, active, setPage }: DataMenuOption) {
   );
 }
 
+type DataAvatar = {
+  img?: string;
+  name: string;
+};
+
+function Avatar({ img, name }: DataAvatar) {
+  return <div></div>;
+}
+
 export default function SectionMenuDashBoard(data: DataSectionMenuDashBoard) {
   const { initialPage } = data;
 
   const [page, setPage] = React.useState<string>(initialPage);
+  const [themeMode, setThemeMode] = React.useState<"light" | "dark">("dark");
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (themeMode === "light") {
+      root.style.setProperty("--primary-color", "#ff0000");
+      root.style.setProperty("--secundary-color", "#1e1e1e");
+      root.style.setProperty("--tertiary-color", "#f5f5f5");
+      root.style.setProperty("--secundary-color-light", "#343434");
+    }
+
+    if (themeMode === "dark") {
+      root.style.setProperty("--primary-color", "#ff0000");
+      root.style.setProperty("--secundary-color", "#f5f5f5");
+      root.style.setProperty("--tertiary-color", "#1e1e1e");
+      root.style.setProperty("--secundary-color-light", "#d5d5d5");
+    }
+  }, [themeMode]);
 
   return (
     <section id={style.SectionMenuDashBoard}>
       <div className={style.headerMenu}>
         <img className={style.mainIcon} src={mainIcon.src} alt="Icone" />
-        <img className={style.logo} src={logo.src} alt="Logo" />
+        <img
+          className={style.logo}
+          src={themeMode === "light" ? logo.src : logoDark.src}
+          alt="Logo"
+        />
       </div>
 
       <div className={style.bodyMenu}>
@@ -74,10 +115,15 @@ export default function SectionMenuDashBoard(data: DataSectionMenuDashBoard) {
               link: "minhas-compras",
               icon: <InventoryIcon />,
             },
-          ] as Array<DataMenuOption>
+            {
+              text: "Configurações",
+              link: "configuracoes",
+              icon: <SettingsIcon />,
+            },
+          ] as Array<DataMenuOptionPage>
         ).map(({ text, link, icon }, index) => {
           return (
-            <MenuOption
+            <MenuOptionPage
               key={index}
               text={text}
               link={link}
@@ -89,17 +135,19 @@ export default function SectionMenuDashBoard(data: DataSectionMenuDashBoard) {
         })}
       </div>
 
-      {/* <div className={style.footerMenu}>
-        <li>
-          <Button variant="contained">Tema claro/escuro</Button>
-        </li>
-        <li>
-          <Link href="/painel/configuracoes">Ir para Configurações</Link>
-        </li>
-        <li>
-          <Link href="/painel/perfil">Ir para Perfil</Link>
-        </li>
-      </div> */}
+      <div className={style.footerMenu}>
+        <button
+          className={style.buttonChangeThemeMode}
+          onClick={() => {
+            setThemeMode(themeMode === "light" ? "dark" : "light");
+          }}
+        >
+          {themeMode === "dark" && <WbIncandescentIcon />}
+          {themeMode === "light" && <WbIncandescentOutlinedIcon />}
+        </button>
+
+        <Link href="/painel/perfil"></Link>
+      </div>
     </section>
   );
 }
